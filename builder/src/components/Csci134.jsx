f778e4430ce493f1a282a11ca0e2bfbddffbc56e
Mark Hopkins <markhopkins@Marks-MacBook-Pro.local>
2026-01-23 15:12:23 -0500

    Favicons everywhere.

--- builder/src/components/Csci134.jsx
+++ builder/src/components/Csci134.jsx
@@ -1,80 +1,10 @@
-import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 import courseData from "./csci134.json";
 
-const UpcomingItem = ({ description, deadline, border }) => {
-  return (
-    <div
-      style={{
-        border: "coral",
-        padding: "3px",
-      }}
-    >
-      <div className="csci134-upcoming">{description.toUpperCase()}</div>
-      <div className="csci134-upcoming" style={{ color: "#F8E067" }}>
-        {deadline}
-      </div>
-    </div>
-  );
-};
-
-const getFutureDate = (daysAhead) => {
-  return new Date(Date.now() + daysAhead * 8.64e7).toDateString();
-};
-
-const Upcoming = () => {
-  const daysAhead = 0;
-  const now = new Date(Date.now() + daysAhead * 8.64e7);
-  const assignments = courseData.quizzes
-    .concat(courseData.labs)
-    .concat(courseData.readings)
-    .sort((lab1, lab2) => new Date(lab1.due) - new Date(lab2.due));
-
-  const labsDueSoon = assignments
-    .filter((lab) => now <= new Date(lab.due))
-    .slice(0, 3);
-
-  return (
-    <div
-      className="upcoming"
-      style={{
-        fontSize: "20px",
-      }}
-    >
-      {labsDueSoon.length > 0 ? "coming up..." : null}
-      {labsDueSoon.length > 0 ? (
-        <UpcomingItem
-          key={labsDueSoon[0].id}
-          description={`${labsDueSoon[0].id.split("-")[0]}: ${labsDueSoon[0].title}`}
-          deadline={labsDueSoon[0].due}
-        ></UpcomingItem>
-      ) : null}
-      {labsDueSoon.length > 1 ? (
-        <UpcomingItem
-          key={labsDueSoon[1].id}
-          description={`${labsDueSoon[1].id.split("-")[0]}: ${labsDueSoon[1].title}`}
-          deadline={labsDueSoon[1].due}
-          border="1px solid orange"
-        ></UpcomingItem>
-      ) : null}
-      {labsDueSoon.length > 2 ? (
-        <UpcomingItem
-          key={labsDueSoon[2].id}
-          description={`${labsDueSoon[2].id.split("-")[0]}: ${labsDueSoon[2].title}`}
-          deadline={labsDueSoon[2].due}
-          border="1px solid orange"
-        ></UpcomingItem>
-      ) : null}
-    </div>
-  );
-};
-
 const Emphasis = (props) => {
-  const colors = ["skyblue", "blue"];
+  const colors = ["#6666ff", "aqua"];
   const [fontColor, setFontColor] = useState(colors[0]);
-  let fontSize = 40;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -88,10 +18,9 @@ const Emphasis = (props) => {
 
   return (
     <div
-      className="csci134-course-number"
       style={{
         color: fontColor,
-        fontSize: `${fontSize}px`,
+        fontSize: "20px",
         transition: "color 2s linear",
       }}
     >
@@ -121,9 +50,7 @@ const Welcome = () => {
         }}
       >
         <div>welcome to 134</div>
-        <div style={{ fontSize: "20px", color: "aqua" }}>
-          an introduction to computer science
-        </div>
+        <Emphasis>an introduction to computer science</Emphasis>
       </div>
 
       <TeachingAssistants />
@@ -216,206 +143,6 @@ const TeachingAssistants = () => {
             where="tcl 216/217"
           />
         </div>
-        <div style={{ flexGrow: 1, flexShrink: 1 }}></div>
-      </div>
-    </div>
-  );
-};
-
-const BoxTitle = ({ title, color }) => {
-  return (
-    <div
-      className="csci134-subtitle"
-      style={{
-        fontSize: "35px",
-        color: color,
-      }}
-    >
-      {title}
-    </div>
-  );
-};
-
-const Activity = ({ title, index, link, release }) => {
-  const now = new Date(Date.now());
-  const locked = release ? now <= new Date(release) : true;
-
-  const renderContent = () => {
-    return (
-      <div
-        className={
-          locked ? "csci134-text csci134-locked" : "csci134-text csci134-button"
-        }
-        style={{
-          borderStyle: "solid",
-          margin: "2px",
-          padding: "2px",
-          color: "black",
-        }}
-      >
-        <span>
-          {index + 1}. {title}
-        </span>{" "}
-        {locked ? "🔒" : null}
-      </div>
-    );
-  };
-
-  return !locked ? (
-    <a href={link} target="_blank">
-      {renderContent()}
-    </a>
-  ) : (
-    renderContent()
-  );
-};
-
-const LectureSlides = () => {
-  return (
-    <div
-      className="csci134-colorblock"
-      style={{
-        backgroundColor: "#226622",
-        flexGrow: 6,
-      }}
-    >
-      <BoxTitle title="lectures" color="#ccffcc" />
-      <div style={{ flexGrow: 1, flexShrink: 1 }}></div>
-      <div
-        style={{
-          display: "flex",
-          flexFlow: "column nowrap",
-          justifyContent: "stretch",
-          alignItems: "stretch",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexFlow: "column wrap",
-            gap: "5px",
-            height: window.innerWidth > 800 ? "700px" : null,
-          }}
-        >
-          {courseData.lectures.map((lecture, index) => (
-            <SlideDeck
-              key={lecture.id}
-              index={index}
-              title={lecture.title.toLowerCase()}
-              link={lecture.link}
-              release={lecture.release}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Activities = () => {
-  return (
-    <div
-      className="csci134-colorblock"
-      style={{
-        backgroundColor: "#442244",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexFlow: "column nowrap",
-          justifyContent: "stretch",
-          alignItems: "stretch",
-        }}
-      >
-        <BoxTitle title="activities" color="#ffccff" />
-        <div style={{ flexGrow: 1, flexShrink: 1 }}></div>
-        <div
-          style={{
-            display: "flex",
-            flexFlow: "column wrap",
-            gap: "5px",
-            height: window.innerWidth > 800 ? "600px" : null,
-          }}
-        >
-          {courseData.activities.map((activity, index) => (
-            <Activity
-              key={activity.id}
-              index={index}
-              title={activity.title.toLowerCase()}
-              link={activity.link}
-              release={activity.release}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Lab = ({ title, index, link, due, release }) => {
-  const now = new Date(Date.now());
-  const locked = release ? now <= new Date(release) : true;
-
-  const renderContent = () => {
-    return (
-      <div
-        className={
-          locked ? "csci134-text csci134-locked" : "csci134-text csci134-button"
-        }
-        style={{
-          borderStyle: "solid",
-          margin: "2px",
-          padding: "2px",
-          color: "black",
-        }}
-      >
-        <span>
-          {index}. {title}
-        </span>{" "}
-        {locked ? "🔒" : null}
-      </div>
-    );
-  };
-
-  return !locked ? (
-    <a href={link} target="_blank">
-      {renderContent()}
-    </a>
-  ) : (
-    renderContent()
-  );
-};
-
-const LabAssignments = () => {
-  return (
-    <div
-      className="csci134-colorblock"
-      style={{
-        backgroundColor: "navy",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexFlow: "column wrap",
-          gap: "5px",
-          justifyContent: "stretch",
-          alignContent: "stretch",
-          height: "100%",
-        }}
-      >
-        <BoxTitle title="labs" color="#aaffff" />
-        {courseData.labs.map((lab, index) => (
-          <Lab
-            key={lab.id}
-            index={index}
-            title={lab.title.toLowerCase()}
-            link={lab.link}
-            due={lab.due}
-            release={lab.release}
-          ></Lab>
-        ))}
         <div style={{ flexGrow: 1, flexShrink: 1 }}></div>
       </div>
     </div>
